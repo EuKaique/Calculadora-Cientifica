4f92259175c4cb8cf846aed42e1c7c44282c629a
Kaique <34290569+EuKaique@users.noreply.github.com>
2022-02-23 17:02:23 -0300

    commit-23-02-2022

--- public/script.js
+++ public/script.js
@@ -44,49 +44,210 @@ $(function(){
 
 });
 
+var visorZero = $('.visor-zero').text('0');
+
 function teclado(num){
-    /*
-    if(num == 7){
-        numeros[0] = 7;
-
-    }else if(num == 8){
-        numeros[1] = 8;
-    }
-    */
-    //var colecao = numeros.values();
-
-    var expressao = document.querySelector('.visor-label').innerHTML;
+    
+    var expressao = $('.visor-label').html();
     var visorLabel = $('.visor-label');
     var formula = $('#calculo');
 
+    visorZero.css('display','none');
+    
     visorLabel.text(expressao + num);
+
     formula.text(expressao + num);
     
-    /*
-    numeros.forEach(function () {
-        $('.visor-label').attr('label');
-        $('label').text(num);
-    });
-    */
-   
+    
     
 }
 
+//Faz o fatorial
+$(document).on('click','#rcFact', function(){
+    valor = Number($('.visor-label').html());
+
+    var fatorial = 1;
+
+    for(i=valor; i > 1; i--){
+        fatorial = fatorial * i; 
+    }
+    
+    $('#calculo').text(valor + '!');
+    $('.visor-label').html(fatorial.toString().replace('.',','));
+    
+
+});
+
+//Faz a raiz quadrada
+$(document).on('click','#rcRaiz', function(){
+    valor = Number($('.visor-label').html());
+
+    raiz = Math.sqrt(valor);
+    
+    $('#calculo').text('√' + valor);
+    $('.visor-label').html(raiz.toString().replace('.',','));
+    
+});
+
+//Faz o logaritmo
+$(document).on('click','#rcLog', function(){
+    valor = parseFloat($('.visor-label').html());
+
+    //log = Math.log(valor) / Math.log(2.71828182845904523);
+    log = Math.log(valor);
+
+    $('#calculo').text(valor + 'log');
+    $('.visor-label').html(log.toString().replace('.',','));
+    
+});
+
+//Faz o ln
+$(document).on('click','#rcLn', function(){
+    valor = parseFloat($('.visor-label').html());
+
+    ln = Math.log(valor) / Math.log(2.71828182845904523);
+
+    $('#calculo').text(valor + 'ln');
+    $('.visor-label').html(ln.toString().replace('.',','));
+    
+});
+
+//Faz a fração
+$(document).on('click','#rcFra', function(){
+    valor = parseFloat($('.visor-label').html());
+
+    fracao = 1 / valor;
+    
+    $('#calculo').text(1 + '/' + valor);
+    $('.visor-label').html(fracao.toString().replace('.',','));
+    
+});
+
+//Faz o seno
+$(document).on('click','#rcSeno', function(){
+    valor = parseFloat($('.visor-label').html());
+
+    seno = Math.sin(valor);
+    
+    $('#calculo').text(valor + 'sen');
+    $('.visor-label').html(seno.toString().replace('.',','));
+    
+});
+
+//Faz o coseno
+$(document).on('click','#rcCos', function(){
+    valor = parseFloat($('.visor-label').html());
+
+    coseno = Math.cos(valor);
+    
+    $('#calculo').text(valor + 'cos');
+    $('.visor-label').html(coseno.toString().replace('.',','));
+    
+});
+
+//Faz a tangente
+$(document).on('click','#rcTan', function(){
+    valor = parseFloat($('.visor-label').html());
+
+    tangente = Math.tan(valor);
+    
+    $('#calculo').text(valor + 'tan');
+    $('.visor-label').html(tangente.toString().replace('.',','));
+    
+});
+
+//Faz o exponencial
+$(document).on('click','#rcExp', function(){
+    valor = $('.visor-label').html();
+    
+    $('#calculo').text(valor + ',e+');
+    $('.visor-label').html(valor + ',e+');
+    
+});
+
+//Faz o euler
+$(document).on('click','#rcE', function(){
+    valor = parseFloat($('.visor-label').html());
+
+    euler = (valor) * 2.71828182846;
+
+    $('#calculo').text(valor + 'e');
+    $('.visor-label').html(euler.toString().replace('.',','));
+    
+});
+
+//Faz o euler elevado
+$(document).on('click','#rce-elevado', function(){
+    valor = parseFloat($('.visor-label').html());
+
+    euler = 2.71828182846;
+    eulerElevado = Math.pow(euler,valor);
+
+    $('#calculo').text('e-elevado a ' + valor);
+    $('.visor-label').html(eulerElevado.toString().replace('.',','));
+    
+});
+
+//Faz o phi
+$(document).on('click','#rcTT', function(){
+    valor = parseFloat($('.visor-label').html());
+
+    phi = (valor) * 3.14159265359;
+
+    $('#calculo').text(valor + 'TT');
+    $('.visor-label').html(phi.toString().replace('.',','));
+    
+});
+
+//Faz a porcentagem
+$(document).on('click','#rcPercentage', function(){
+    valor = parseInt($('.visor-label').html());
+
+    porcentagem = valor / 100;
+
+    $('#calculo').text(valor + '%');
+    $('.visor-label').html(porcentagem.toString().replace('.',','));
+    
+});
+
+//Faz o 10 elevado a x
+$(document).on('click','#rcDez', function(){
+    valor = parseInt($('.visor-label').html());
+
+    dezElevado = Math.pow(10,valor);
+
+    $('#calculo').text('10 elevado a ' + valor);
+    $('.visor-label').html(dezElevado.toString().replace('.',','));
+    
+});
+
+
+
 function limparTudo(){
+    visorZero.css('display','inline-block');
+    visorZero.text('0');
     $('.visor-label').text('');
     $('#calculo').text('');
 }
 
 function limparUm(){
-    var expressao = document.querySelector('.visor-label').innerHTML;
-    
-    document.querySelector('.visor-label').innerHTML = expressao.substring(0, expressao.length -1);
+    var expressao = $('.visor-label').html();
+
+    if(expressao != '0' && expressao.length != 1){
+        $('.visor-label').html(expressao.substring(0, expressao.length -1));
+    }
+    if(expressao.length == 1){
+        $('.visor-label').text('0');
+    }
+
 }
 
 function calcular(){
     var expressao = document.querySelector('.visor-label').innerHTML;
 
     if(expressao){
+ 
         document.querySelector('.visor-label').innerHTML = eval(expressao);
     }
 }
+
