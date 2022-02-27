01b06823e327632a74fb8e1d63e96a87b4111448
Kaique <34290569+EuKaique@users.noreply.github.com>
2022-02-26 23:28:43 -0300

    commit-26-02-2022

--- public/script.js
+++ public/script.js
@@ -7,10 +7,38 @@ $(function(){
     var botoes = $('.botoes');
     var visor = $('.visor');
     var textoCalculo = $('#text-calculo');
-    
-    //var virgula = $('#rcVir');
+    var telaCalculadora = window.screen.width;
+    var rceElevado = $('#rce-elevado');
+    var rcDez = $('#rcDez');
+    var rcFact = $('#rcFact');
+    var rcLn = $('#rcLn');
+    var rcLog = $('#rcLog');
+    var rcSeno = $('#rcSeno');
+    var rcCos = $('#rcCos');
+    var rcRaiz = $('#rcRaiz');
+    var rcPot = $('#rcPot');
+    var rcTan = $('#rcTan');
+    var rcFra = $('#rcFra');
+    var rcExp = $('#rcExp');
+    var rcTT = $('#rcTT');
+    var rcE = $('#rcE');
+    var rcPon = $('#rcPon');
+    var rcPercentage = $('#rcPercentage');
+    var rc9 = $('#rc9');
+    var rc8 = $('#rc8');
+    var rc7 = $('#rc7');
+    var rc6 = $('#rc6');
+    var rc5 = $('#rc5');
+    var rc4 = $('#rc4');
+    var rc3 = $('#rc3');
+    var rc2 = $('#rc2');
+    var rc1 = $('#rc1');
+    var rc0 = $('#rc0');
+    var rcParenA = $('#rcParenA');
+    var rcParenB = $('#rcParenB');
+    var rcC = $('#rcC');
+    var rcBack = $('#rcBack');
 
-    
     //Adiciona e remove a classe ativo, mostra e oculta funções das calculadoras
     btnComum.on('click',function(){
         btnCientifica.removeClass('ativo');
@@ -24,7 +52,43 @@ $(function(){
         botoes.css('margin-left','0px');
         btnCientifica.css('margin-left','40px');
         btnComum.css('margin-left','10px');
-        textoCalculo.css('margin-left','28%');
+
+        if(telaCalculadora <= '1000' && telaCalculadora > '800'){
+            calculadora.width('28rem');
+            visor.width('78%');
+            botoes.css('margin-left','0px');
+            botoes.css('margin-top','0.5rem');
+            btnCientifica.css('margin-left','0px');
+            btnComum.css('margin-left','0px');
+            textoCalculo.css('left','20%');
+            textoCalculo.css('top','4.5rem');
+            textoCalculo.css('position','relative');
+        }
+        if(telaCalculadora <= '1620' && telaCalculadora > '1000'){
+            botoes.css('margin-left','3.5rem');
+            btnCientifica.css('margin-left','0px');
+            btnComum.css('margin-left','0px');
+            textoCalculo.css('margin-left','28%');
+        }
+        if(telaCalculadora <= '800' && telaCalculadora > '600'){
+            visor.width('100%');
+            botoes.css('margin-top','0.8rem');
+            textoCalculo.css('margin-left','31%');
+        }
+        if(telaCalculadora <= '600' && telaCalculadora > '500'){
+            calculadora.width('18rem');
+            visor.width('92%');
+            textoCalculo.css('margin-left','26%');
+            $('#btn-cientifica').css('margin-left','0');
+            $('#btn-cientifica').css('margin-right','0');
+        }
+        if(telaCalculadora <= '500' && telaCalculadora > '400'){
+            calculadora.width('18rem');
+            visor.width('92%');
+            textoCalculo.css('margin-left','26%');
+            $('#btn-cientifica').css('margin-left','0');
+            $('#btn-cientifica').css('margin-right','0');
+        }
     });
 
     btnCientifica.on('click',function(){
@@ -34,15 +98,345 @@ $(function(){
         tecladoCientifica.fadeIn(400);
 
         //trata as dimensões da calculadora
+        if(telaCalculadora <= '1620' && telaCalculadora > '1000'){
+            calculadora.width('80%');
+            visor.width('61%');
+            botoes.css('margin-left','3.5rem');
+            btnCientifica.css('margin-left','0px');
+            btnComum.css('margin-left','0px');
+            textoCalculo.css('margin-left','16%');
+        }
+        if(telaCalculadora <= '1000' && telaCalculadora > '800'){
+            calculadora.width('53rem');
+            visor.width('61%');
+            botoes.css('margin-left','3.5rem');
+            btnCientifica.css('margin-left','0px');
+            btnComum.css('margin-left','0px');
+            textoCalculo.css('left','0%');
+        }
+        if(telaCalculadora <= '800' && telaCalculadora > '600'){
+            calculadora.width('46rem');
+            calculadora.height('28rem');
+            botoes.css('margin-left','0.95rem');
+            btnCientifica.css('margin-left','0px');
+            btnComum.css('margin-left','0px');
+            textoCalculo.css('margin-left','9%');
+            textoCalculo.css('top','4.5rem');
+            textoCalculo.css('position','relative');
+            visor.width('56%');
+            rceElevado.css('padding','0.4rem 2.3rem');
+            rcLn.css('padding','0.5rem 2.2rem');
+            rcLog.css('padding','0.5rem 1.8rem');
+            rcExp.css('padding','0.5rem 1.8rem');
+            rcPot.css('padding','0.5rem 2.5rem');
+            rcDez.css('padding','0.5rem 2.4rem');
+            rcSeno.css('padding','0.5rem 2rem');
+            rcCos.css('padding','0.5rem 2.3rem');
+            rcTan.css('padding','0.5rem 2.4rem');
+            rcTT.css('padding','0.5rem 2.6rem');
+            rcFact.css('padding','0.5rem 2rem');
+            rcPercentage.css('padding','0.5rem 2rem');
+            rcRaiz.css('padding','0.5rem 2.2rem');
+            rcFra.css('padding','0.5rem 1.6rem');
+            rcE.css('padding','0.5rem 2.2rem');
+            rcParenA.css('padding','0.5rem 2rem');
+            rcParenB.css('padding','0.5rem 2rem');
+            rcC.css('padding','0.5rem 2rem');
+            rcBack.css('padding','0.5rem 1rem');
+            rc9.css('padding','0.5rem 2rem');
+            rc8.css('padding','0.5rem 2rem');
+            rc7.css('padding','0.5rem 2rem');
+            rc6.css('padding','0.5rem 2rem');
+            rc5.css('padding','0.5rem 2rem');
+            rc4.css('padding','0.5rem 2rem');
+            rc3.css('padding','0.5rem 2rem');
+            rc2.css('padding','0.5rem 2rem');
+            rc1.css('padding','0.5rem 2rem');
+            rc0.css('padding','0.5rem 2rem');
+            rcPon.css('padding','0.5rem 2.2rem');
+            rcIgual.css('padding','0.86rem 2rem');
+        }
+        if(telaCalculadora <= '600' && telaCalculadora > '500'){
+            calculadora.width('30rem');
+            calculadora.height('28rem');
+            botoes.css('margin-left','0');
+            botoes.css('margin-top','0.7rem');
+            btnCientifica.css('margin-left','0px');
+            btnComum.css('margin-left','0px');
+            textoCalculo.css('margin-left','9%');
+            textoCalculo.css('top','4.5rem');
+            textoCalculo.css('position','relative');
+            visor.width('100%');
+            rceElevado.css('padding','0.4rem 1.3rem');
+            rcLn.css('padding','0.5rem 1.2rem');
+            rcLog.css('padding','0.5rem 0.9rem');
+            rcExp.css('padding','0.5rem 0.9rem');
+            rcPot.css('padding','0.5rem 1.5rem');
+            rcDez.css('padding','0.5rem 1.4rem');
+            rcSeno.css('padding','0.5rem 1rem');
+            rcCos.css('padding','0.5rem 1.3rem');
+            rcTan.css('padding','0.5rem 1.4rem');
+            rcTT.css('padding','0.5rem 1.6rem');
+            rcFact.css('padding','0.5rem 1rem');
+            rcPercentage.css('padding','0.5rem 1rem');
+            rcRaiz.css('padding','0.5rem 1.2rem');
+            rcFra.css('padding','0.5rem 0.6rem');
+            rcE.css('padding','0.5rem 1.2rem');
+            rcParenA.css('padding','0.5rem 1rem');
+            rcParenB.css('padding','0.5rem 1rem');
+            rcC.css('padding','0.5rem 1rem');
+            rcBack.css('padding','0.5rem 1rem');
+            rc9.css('padding','0.5rem 1rem');
+            rc8.css('padding','0.5rem 1rem');
+            rc7.css('padding','0.5rem 1rem');
+            rc6.css('padding','0.5rem 1rem');
+            rc5.css('padding','0.5rem 1rem');
+            rc4.css('padding','0.5rem 1rem');
+            rc3.css('padding','0.5rem 1rem');
+            rc2.css('padding','0.5rem 1rem');
+            rc1.css('padding','0.5rem 1rem');
+            rc0.css('padding','0.5rem 1rem');
+            rcPon.css('padding','0.5rem 1.2rem');
+            rcIgual.css('padding','0.86rem 1rem');
+        }
+        if(telaCalculadora <= '500' && telaCalculadora > '380'){
+            calculadora.width('30rem');
+            calculadora.height('28rem');
+            botoes.css('margin-left','0');
+            botoes.css('margin-top','0.7rem');
+            btnCientifica.css('margin-left','0px');
+            btnComum.css('margin-left','0px');
+            textoCalculo.css('margin-left','9%');
+            textoCalculo.css('top','4.5rem');
+            textoCalculo.css('position','relative');
+            visor.width('100%');
+            rceElevado.css('padding','0.4rem 1.3rem');
+            rcLn.css('padding','0.5rem 1.2rem');
+            rcLog.css('padding','0.5rem 0.9rem');
+            rcExp.css('padding','0.5rem 0.9rem');
+            rcPot.css('padding','0.5rem 1.5rem');
+            rcDez.css('padding','0.5rem 1.4rem');
+            rcSeno.css('padding','0.5rem 1rem');
+            rcCos.css('padding','0.5rem 1.3rem');
+            rcTan.css('padding','0.5rem 1.4rem');
+            rcTT.css('padding','0.5rem 1.6rem');
+            rcFact.css('padding','0.5rem 1rem');
+            rcPercentage.css('padding','0.5rem 1rem');
+            rcRaiz.css('padding','0.5rem 1.2rem');
+            rcFra.css('padding','0.5rem 0.6rem');
+            rcE.css('padding','0.5rem 1.2rem');
+            rcParenA.css('padding','0.5rem 1rem');
+            rcParenB.css('padding','0.5rem 1rem');
+            rcC.css('padding','0.5rem 1rem');
+            rcBack.css('padding','0.5rem 1rem');
+            rc9.css('padding','0.5rem 1rem');
+            rc8.css('padding','0.5rem 1rem');
+            rc7.css('padding','0.5rem 1rem');
+            rc6.css('padding','0.5rem 1rem');
+            rc5.css('padding','0.5rem 1rem');
+            rc4.css('padding','0.5rem 1rem');
+            rc3.css('padding','0.5rem 1rem');
+            rc2.css('padding','0.5rem 1rem');
+            rc1.css('padding','0.5rem 1rem');
+            rc0.css('padding','0.5rem 1rem');
+            rcPon.css('padding','0.5rem 1.2rem');
+            rcIgual.css('padding','0.86rem 1rem');
+        }
+
+    });
+
+});
+
+// Trata o tamanho da calculadora conforme a tela
+
+function tela(){
+    var btnComum = $('#btn-comum');
+    var btnCientifica = $('#btn-cientifica');
+    var tecladoCientifica = $('.cal-cientifica');
+    var calculadora = $('.calculadora');
+    var botoes = $('.botoes');
+    var visor = $('.visor');
+    var textoCalculo = $('#text-calculo');
+    var telaCalculadora = window.screen.width;
+    var rceElevado = $('#rce-elevado');
+    var rcDez = $('#rcDez');
+    var rcFact = $('#rcFact');
+    var rcLn = $('#rcLn');
+    var rcLog = $('#rcLog');
+    var rcSeno = $('#rcSeno');
+    var rcCos = $('#rcCos');
+    var rcRaiz = $('#rcRaiz');
+    var rcPot = $('#rcPot');
+    var rcTan = $('#rcTan');
+    var rcFra = $('#rcFra');
+    var rcExp = $('#rcExp');
+    var rcTT = $('#rcTT');
+    var rcE = $('#rcE');
+    var rcPon = $('#rcPon');
+    var rcPercentage = $('#rcPercentage');
+    var rc9 = $('#rc9');
+    var rc8 = $('#rc8');
+    var rc7 = $('#rc7');
+    var rc6 = $('#rc6');
+    var rc5 = $('#rc5');
+    var rc4 = $('#rc4');
+    var rc3 = $('#rc3');
+    var rc2 = $('#rc2');
+    var rc1 = $('#rc1');
+    var rc0 = $('#rc0');
+    var rcParenA = $('#rcParenA');
+    var rcParenB = $('#rcParenB');
+    var rcC = $('#rcC');
+    var rcBack = $('#rcBack');
+    var rcIgual = $('#rcIgual');
+    
+    if(telaCalculadora <= '1280' && telaCalculadora > '1000'){
         calculadora.width('80%');
         visor.width('61%');
         botoes.css('margin-left','3.5rem');
         btnCientifica.css('margin-left','0px');
         btnComum.css('margin-left','0px');
         textoCalculo.css('margin-left','16%');
-    });
+    }
+    
+    if(telaCalculadora <= '1000' && telaCalculadora > '800'){
+        calculadora.width('53rem');
+        calculadora.height('28rem');
+        botoes.css('margin-left','3.5rem');
+        btnCientifica.css('margin-left','0px');
+        btnComum.css('margin-left','0px');
+        textoCalculo.css('margin-left','8%');
+    }
 
-});
+    if(telaCalculadora <= '800' && telaCalculadora > '600'){
+        calculadora.width('46rem');
+        calculadora.height('28rem');
+        botoes.css('margin-left','0.95rem');
+        btnCientifica.css('margin-left','0px');
+        btnComum.css('margin-left','0px');
+        textoCalculo.css('margin-left','9%');
+        textoCalculo.css('top','4.5rem');
+        textoCalculo.css('position','relative');
+        visor.width('56%');
+        rceElevado.css('padding','0.4rem 2.3rem');
+        rcLn.css('padding','0.5rem 2.2rem');
+        rcLog.css('padding','0.5rem 1.8rem');
+        rcExp.css('padding','0.5rem 1.8rem');
+        rcPot.css('padding','0.5rem 2.5rem');
+        rcDez.css('padding','0.5rem 2.4rem');
+        rcSeno.css('padding','0.5rem 2rem');
+        rcCos.css('padding','0.5rem 2.3rem');
+        rcTan.css('padding','0.5rem 2.4rem');
+        rcTT.css('padding','0.5rem 2.6rem');
+        rcFact.css('padding','0.5rem 2rem');
+        rcPercentage.css('padding','0.5rem 2rem');
+        rcRaiz.css('padding','0.5rem 2.2rem');
+        rcFra.css('padding','0.5rem 1.6rem');
+        rcE.css('padding','0.5rem 2.2rem');
+        rcParenA.css('padding','0.5rem 2rem');
+        rcParenB.css('padding','0.5rem 2rem');
+        rcC.css('padding','0.5rem 2rem');
+        rcBack.css('padding','0.5rem 1rem');
+        rc9.css('padding','0.5rem 2rem');
+        rc8.css('padding','0.5rem 2rem');
+        rc7.css('padding','0.5rem 2rem');
+        rc6.css('padding','0.5rem 2rem');
+        rc5.css('padding','0.5rem 2rem');
+        rc4.css('padding','0.5rem 2rem');
+        rc3.css('padding','0.5rem 2rem');
+        rc2.css('padding','0.5rem 2rem');
+        rc1.css('padding','0.5rem 2rem');
+        rc0.css('padding','0.5rem 2rem');
+        rcPon.css('padding','0.5rem 2.2rem');
+        rcIgual.css('padding','0.86rem 2rem');
+    }
+    if(telaCalculadora <= '600' && telaCalculadora > '500'){
+        calculadora.width('30rem');
+        calculadora.height('28rem');
+        botoes.css('margin-left','0');
+        botoes.css('margin-top','0.7rem');
+        btnCientifica.css('margin-left','0px');
+        btnComum.css('margin-left','0px');
+        textoCalculo.css('margin-left','9%');
+        textoCalculo.css('top','4.5rem');
+        textoCalculo.css('position','relative');
+        visor.width('100%');
+        rceElevado.css('padding','0.4rem 1.3rem');
+        rcLn.css('padding','0.5rem 1.2rem');
+        rcLog.css('padding','0.5rem 0.9rem');
+        rcExp.css('padding','0.5rem 0.9rem');
+        rcPot.css('padding','0.5rem 1.5rem');
+        rcDez.css('padding','0.5rem 1.4rem');
+        rcSeno.css('padding','0.5rem 1rem');
+        rcCos.css('padding','0.5rem 1.3rem');
+        rcTan.css('padding','0.5rem 1.4rem');
+        rcTT.css('padding','0.5rem 1.6rem');
+        rcFact.css('padding','0.5rem 1rem');
+        rcPercentage.css('padding','0.5rem 1rem');
+        rcRaiz.css('padding','0.5rem 1.2rem');
+        rcFra.css('padding','0.5rem 0.6rem');
+        rcE.css('padding','0.5rem 1.2rem');
+        rcParenA.css('padding','0.5rem 1rem');
+        rcParenB.css('padding','0.5rem 1rem');
+        rcC.css('padding','0.5rem 1rem');
+        rcBack.css('padding','0.5rem 1rem');
+        rc9.css('padding','0.5rem 1rem');
+        rc8.css('padding','0.5rem 1rem');
+        rc7.css('padding','0.5rem 1rem');
+        rc6.css('padding','0.5rem 1rem');
+        rc5.css('padding','0.5rem 1rem');
+        rc4.css('padding','0.5rem 1rem');
+        rc3.css('padding','0.5rem 1rem');
+        rc2.css('padding','0.5rem 1rem');
+        rc1.css('padding','0.5rem 1rem');
+        rc0.css('padding','0.5rem 1rem');
+        rcPon.css('padding','0.5rem 1.2rem');
+        rcIgual.css('padding','0.86rem 1rem');
+    }
+    if(telaCalculadora <= '500' && telaCalculadora > '380'){
+        calculadora.width('30rem');
+        calculadora.height('28rem');
+        botoes.css('margin-left','0');
+        botoes.css('margin-top','0.7rem');
+        btnCientifica.css('margin-left','0px');
+        btnComum.css('margin-left','0px');
+        textoCalculo.css('margin-left','9%');
+        textoCalculo.css('top','4.5rem');
+        textoCalculo.css('position','relative');
+        visor.width('100%');
+        rceElevado.css('padding','0.4rem 1.3rem');
+        rcLn.css('padding','0.5rem 1.2rem');
+        rcLog.css('padding','0.5rem 0.9rem');
+        rcExp.css('padding','0.5rem 0.9rem');
+        rcPot.css('padding','0.5rem 1.5rem');
+        rcDez.css('padding','0.5rem 1.4rem');
+        rcSeno.css('padding','0.5rem 1rem');
+        rcCos.css('padding','0.5rem 1.3rem');
+        rcTan.css('padding','0.5rem 1.4rem');
+        rcTT.css('padding','0.5rem 1.6rem');
+        rcFact.css('padding','0.5rem 1rem');
+        rcPercentage.css('padding','0.5rem 1rem');
+        rcRaiz.css('padding','0.5rem 1.2rem');
+        rcFra.css('padding','0.5rem 0.6rem');
+        rcE.css('padding','0.5rem 1.2rem');
+        rcParenA.css('padding','0.5rem 1rem');
+        rcParenB.css('padding','0.5rem 1rem');
+        rcC.css('padding','0.5rem 1rem');
+        rcBack.css('padding','0.5rem 1rem');
+        rc9.css('padding','0.5rem 1rem');
+        rc8.css('padding','0.5rem 1rem');
+        rc7.css('padding','0.5rem 1rem');
+        rc6.css('padding','0.5rem 1rem');
+        rc5.css('padding','0.5rem 1rem');
+        rc4.css('padding','0.5rem 1rem');
+        rc3.css('padding','0.5rem 1rem');
+        rc2.css('padding','0.5rem 1rem');
+        rc1.css('padding','0.5rem 1rem');
+        rc0.css('padding','0.5rem 1rem');
+        rcPon.css('padding','0.5rem 1.2rem');
+        rcIgual.css('padding','0.86rem 1rem');
+    }
+}
 
 var visorZero = $('.visor-zero').text('0');
 
